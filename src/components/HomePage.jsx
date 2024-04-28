12140e6ddd439c86e8aea432b9c28ea4b2d6394a
Neeraj Yadav <neeraj95578@gmail.com>
2024-04-28 22:33:32 +0530

    first commit

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const HomePage = () => {
+  const [input, setInput] = useState("");
+
+  const navigate = useNavigate();
+
+  const submitHandler = () => {
+    navigate(`/room/${input}`);
+  };
+  return (
+    <div>
+      <input
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        type="text"
+        placeholder="Enter your name..."
+      />
+      <button onClick={submitHandler}>Join</button>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/components/constant.js
@@ -0,0 +1,2 @@
+export const API_ID = 500733958;
+export const SERVER_SECRET = "d1514b3516582f4a9deefda753f567ba";
